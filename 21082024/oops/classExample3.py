030ef7c5ff6ae762f1343e269b93f402602b514d
gkrishna247 <krishnamoorthitech2224@gmail.com>
2024-08-21 14:48:36 +0530

    new class example Added

---
+++ 21082024/oops/classExample3.py
@@ -0,0 +1,12 @@
+class maths:
+    def square(self,a):
+        b=a*a
+        return b
+    def sum(self,x,y):
+        h=x+y
+        print("sum: ",h)
+    
+m=maths()
+print(m.square(10))
+m.sum(2,3)
+
