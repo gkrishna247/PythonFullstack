c6a4dff3d0077c233572691b259bb479838c8090
gkrishna247 <krishnamoorthitech2224@gmail.com>
2024-08-27 14:18:10 +0530

    new example for multiple properties of class added

---
+++ 27082024/example02.py
@@ -0,0 +1,17 @@
+class a:
+    def one(self):
+        self.f=10
+
+    def two(self):
+        self.k=20
+
+    def three(self):
+        self.j=30
+
+r=a()
+r.one()
+r.two()
+r.three()
+print(r.f)
+print(r.k)
+print(r.j)
