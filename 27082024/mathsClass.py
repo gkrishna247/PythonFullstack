7252bb68044181213764b7c61ee8ada466ddf6cb
gkrishna247 <krishnamoorthitech2224@gmail.com>
2024-08-27 10:28:01 +0530

    updated file mathsClass.py

--- 27082024/mathsClass.py
+++ 27082024/mathsClass.py
@@ -13,9 +13,15 @@ class maths:
         self.x=int(input("Enter value of x: "))
         self.y=int(input("Enter value of y: "))
         return self.x+self.y
+    
+    def addArgRt(self,n1,n2):
+        return n1+n2
 
 t=maths()
 print("Total: ",t.z)
 t.add()
 print("Total: ",t.z)
 print("Total: ",t.addrt())
+num1=int(input("Enter first value: "))
+num2=int(input("Enter second value: "))
+print("Total: ",t.addArgRt(num1,num2))
