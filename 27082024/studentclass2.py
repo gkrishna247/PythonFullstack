e5dbbcd5bec968d9bfc6827d757cde06a3504011
gkrishna247 <krishnamoorthitech2224@gmail.com>
2024-08-27 14:35:52 +0530

    New examples Added

---
+++ 27082024/studentclass2.py
@@ -0,0 +1,10 @@
+class student:
+    def __init__(self):
+        self.adno=int(input("Adno: "))
+        self.name=input("Name: ")
+        self.address=input("Address: ")
+
+t=[]
+t.append(student())
+t.append(student())
+t.append(student())
