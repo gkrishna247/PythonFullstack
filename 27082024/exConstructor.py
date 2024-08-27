5008fe891b7866b462c00a6d0203a908a21cf46d
gkrishna247 <krishnamoorthitech2224@gmail.com>
2024-08-27 09:56:06 +0530

    exconstructor.py updated the concept default constructor

--- 27082024/exConstructor.py
+++ 27082024/exConstructor.py
@@ -1,6 +1,7 @@
 class employee:
     def __init__(self,x):
-        self.x=10
-
+        self.x=10   #parameter constructor
+        self.y=20   #Default Constructor
+    
 n=employee(10)
 print(n.x)
