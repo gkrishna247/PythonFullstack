311d3d44a6370517b65945c8b37a5ffed378c18b
gkrishna247 <krishnamoorthitech2224@gmail.com>
2024-08-27 10:23:36 +0530

    updated file mathsClass.py

--- 27082024/mathsClass.py
+++ 27082024/mathsClass.py
@@ -4,7 +4,18 @@ class maths:
         self.y=int(input("Enter value of y: "))
         self.z=self.x+self.y
 
+    def __init__(self):
+        self.x=int(input("Enter value of x: "))
+        self.y=int(input("Enter value of y: "))
+        self.z=self.x+self.y
+
+    def addrt(self):
+        self.x=int(input("Enter value of x: "))
+        self.y=int(input("Enter value of y: "))
+        return self.x+self.y
+
 t=maths()
+print("Total: ",t.z)
 t.add()
 print("Total: ",t.z)
-        
+print("Total: ",t.addrt())
