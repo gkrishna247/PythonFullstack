842e260771b52d8cec09e7b14dc96579dde7a086
gkrishna247 <krishnamoorthitech2224@gmail.com>
2024-08-28 11:53:32 +0530

    new example for anonymous obj is updated

--- 28082024/anonyobj03.py
+++ 28082024/anonyobj03.py
@@ -7,9 +7,14 @@ class student:
         self.name=input("Name: ")
 
 listt=[]
-listt.append(student())
-listt.append(student())
-listt.append(student())
+i=1
+while(i<=1):
+    status=int(input("Do you want to exit.press 3"))
+    if(status==3):
+        break
+    else:
+        listt.append(student())
+
 for i in listt:
     print("Ad.No: ",i.adno)
     print("Name: ",i.name)
