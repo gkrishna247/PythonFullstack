d0fb7d3bc0f5762b86129239f01be1feb20a846d
gkrishna247 <krishnamoorthitech2224@gmail.com>
2024-08-28 11:36:21 +0530

    new example for anonymous obj is updated

--- 28082024/anonyobj03.py
+++ 28082024/anonyobj03.py
@@ -1,4 +1,7 @@
 class student:
+    clg="ksr college fo technology"
+    dt="nammakkal"
+    pincode="637215"
     def __init__(self):
         self.adno=input("Adno: ")
         self.name=input("Name: ")
@@ -10,3 +13,6 @@ listt.append(student())
 for i in listt:
     print("Ad.No: ",i.adno)
     print("Name: ",i.name)
+    print("CollegeName: ",i.clg)
+    print("District: ",i.dt)
+    print("PinCode: ",i.pincode)
