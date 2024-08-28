c55c9496ef9fa8e85e9d323a6bb53ea55321d7a2
gkrishna247 <krishnamoorthitech2224@gmail.com>
2024-08-28 09:37:55 +0530

    Examples for static class updated

--- 28082024/staticclass01.py
+++ 28082024/staticclass01.py
@@ -1,9 +1,13 @@
 class student:
     a="ksr college of technology"
+    def access(self):
+        self.a="welcome"
 
 print(student.a)
-f=student()
+f=student() #non static object
 m=student()
+
+f.access()
 
 print(f.a)
 print(m.a)
