c73f16736fbb03066d9b19f4ece53d63725c3f85
gkrishna247 <krishnamoorthitech2224@gmail.com>
2024-09-11 14:37:48 +0530

    Example for error handling addedc

---
+++ 11092024/errorhandling/example1.py
@@ -0,0 +1,9 @@
+# Error handling example 1
+# This code will throw an error because we are trying to divide a number by zero
+
+a=1
+b=0
+c=a/b
+print(c)
+# Output
+# ZeroDivisionError: division by zero
