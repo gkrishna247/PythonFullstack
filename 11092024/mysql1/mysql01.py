82c21ea2a2d303acaaacad6fb9fb97264ec37bcf
gkrishna247 <krishnamoorthitech2224@gmail.com>
2024-09-11 13:55:59 +0530

    file updated

--- 11092024/mysql1/mysql01.py
+++ 11092024/mysql1/mysql01.py
@@ -3,6 +3,15 @@ import mysql.connector
 mydb = mysql.connector.connect(host="localhost", user="root", password="", database="dummy")
 
 mycursor = mydb.cursor()
-sql="insert into ksr values(1,'raj','chennai','tamilnadu')"
-mycursor.execute(sql)
-mydb.commit()
+print(my)
+sql1="select * from ksr"
+mycursor.execute(sql1)
+
+# Fetch the result of the query
+result = mycursor.fetchall()
+
+# Print the fetched results
+for row in result:
+    print(row)
+
+print("Data Fetched Successfully")
