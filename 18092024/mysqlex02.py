a8bf5e9ad774d73836b3a36e5b94d9810c15601d
gkrishna247 <krishnamoorthitech2224@gmail.com>
2024-09-18 17:45:06 +0530

    New example for sql is added

---
+++ 18092024/mysqlex02.py
@@ -0,0 +1,31 @@
+#program for performing mysql cred operations
+import mysql.connector
+
+#connecting to mysql
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "",
+    database = "dummy"
+)
+
+#creating cursor object
+mycursor = mydb.cursor()
+
+#sql query
+sql = "insert into ksr (rollno, name, address, city ) values (%s, %s, %s, %s)"
+
+#taking input from user
+rollno = input("Enter Roll No: ")
+name = input("Enter Name: ")
+address = input("Enter Address: ")
+city = input("Enter City: ")
+
+#executing the query
+mycursor.execute(sql, (rollno, name, address, city))
+
+#commiting the changes
+mydb.commit()
+print("Data inserted successfully")
+#closing the connection
+mydb.close()
