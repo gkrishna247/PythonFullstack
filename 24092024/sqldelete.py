0f231608116592afc8acafa1e8ac880e8c248c88
gkrishna247 <krishnamoorthitech2224@gmail.com>
2024-09-24 11:53:39 +0530

    new file to perform sql delete is added

---
+++ 24092024/sqldelete.py
@@ -0,0 +1,16 @@
+#program to perform sql delete
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "",
+    database = "employ"
+)
+
+cursor = mydb.cursor()
+id = input("Enter Empid: ")
+sql = "delete from emp_per where empid = %s"
+cursor.execute(sql, (id,))
+mydb.commit()
+print("Data deleted successfully")
