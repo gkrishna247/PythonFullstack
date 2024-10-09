d3cb91fb3284bf90885023f54ed2c65b806ca27b
gkrishna247 <krishnamoorthitech2224@gmail.com>
2024-10-09 12:18:16 +0530

    Add SQL queries for creating and populating the 'bus' table in the Schooldb database

--- 09102024/mysql01.sql
+++ 09102024/mysql01.sql
@@ -17,4 +17,12 @@ insert into student_details values ("kumar",4,"9876543299");
 
 insert into student_details values ("John",6,"9876543216");
 
+create table bus (bus_id int primary key auto_increment,bus_no int,s_id int,
+foreign key (s_id) references student_details(s_id));
+
+desc bus;
+
+insert into bus values (11,81,1),(12,85,2),(13,21,3),(14,89,4),(15,41,5);
+select * from bus;
+
 commit;
