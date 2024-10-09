fe90734d302eb8c6bc0549293b0d5cbabea53022
gkrishna247 <krishnamoorthitech2224@gmail.com>
2024-10-09 15:35:56 +0530

    Add HR_department and modify emp_details table in mysql02.sql

--- 09102024/mysql02.sql
+++ 09102024/mysql02.sql
@@ -22,8 +22,19 @@ create table HR_department (HR_id int primary key auto_increment,emp_id int,
     
 desc hr_department;
 insert into hr_department values(21,8,"Kumar","HR","HR Manager",60000,"9876543213","kumar001@gmail.com");
+insert into hr_department values
+	(31,6,"Raja","IT","Senior Dev",50000,"9876543211","raja001@gmail.com"),
+    (54,7,"Ram","IT","Junior Dev",30000,"9876543212","ram001@gmail.com"),
+    (34,9,"John","Sales","Sales Rep",25000,"9876543213","john001@gmail.com");
+select * from emp_details;
+select * from hr_department;
+delete from emp_details where emp_id=10;
+insert into emp_details values (10,"Selva","Finance","Auditor",80000,"9876543214","selva001@gmail.com");
+
+delete from emp_details where emp_id=8;
+
+delete from hr_department where emp_id=7;
 
 commit;  
-    
 drop table hr_department;
 set sql_safe_updates=0
