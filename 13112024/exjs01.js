56da230cc6f541aa30a65a3a39c4a69fd259d913
gkrishna247 <krishnamoorthitech2224@gmail.com>
2024-11-13 11:39:01 +0530

    Add exjs01.js to demonstrate object property access using dot and array notation

---
+++ 13112024/exjs01.js
@@ -0,0 +1,13 @@
+const student = {
+    name: "ksr",
+    dept: "Aiml",
+    courses: ["cse", "aids", "aiml", "csbs"]
+};
+
+// dot notation
+let valueDot = student.name;
+console.log(valueDot);
+
+// array notation
+let valueArray = student["name"];
+console.log(valueArray);
